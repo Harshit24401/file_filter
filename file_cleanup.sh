239937d9690d655a7e0524567b76283915e93c3b
Harshit Arora <77843894+Harshit24401@users.noreply.github.com>
2024-10-21 15:52:40 +0530

    Update file_cleanup.sh

--- file_cleanup.sh
+++ file_cleanup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to create a directory if it doesn't exist
 create_dir() {
   if [[ ! -d "$1" ]]; then
     mkdir -p "$1"
@@ -10,7 +9,7 @@ create_dir() {
 # Get the current directory
 current_dir=$(pwd)
 
-# Iterate through all files in the current directory
+# Iterate through all files
 for file in "$current_dir"/*; do
   # Get the file extension
   extension="${file##*.}"
@@ -20,11 +19,10 @@ for file in "$current_dir"/*; do
     continue
   fi
 
-  # Create a directory for the file type if it doesn't exist and it's not a directory itself
+  
   if [[ ! -d "$current_dir/$extension" ]]; then
     create_dir "$current_dir/$extension"
   fi
 
-  # Move the file to the corresponding directory
   mv "$file" "$current_dir/$extension"
 done
